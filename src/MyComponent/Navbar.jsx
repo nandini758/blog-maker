8f8d318a0e20349e2eff988fd64f22744700124f
Nandini <nandinimehta758@gmail.com>
2024-03-17 01:19:32 +0530

    Navigation

--- src/MyComponent/Navbar.jsx
+++ src/MyComponent/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { getAuth } from 'firebase/auth'
 import { Link,useNavigate, useLocation} from 'react-router-dom'
+import "./Nav.css"
 
 const Navbar = () => {
   const navigate=useNavigate();
@@ -18,14 +19,15 @@ const Navbar = () => {
     <>
     <div className='bg-primary  d-flex  align-items-center p-1' style={{justifyContent:"space-between"}}>
       <div className="user-content  d-flex justify-content-center align-items-center">
-      <img src={auth?.currentUser.photoURL} alt="" style={{width:"20%", borderRadius:"50%",marginRight:"1rem"}}/>
-      <h3>{auth?.currentUser.displayName}</h3>
+      <img id="company-logo" src="https://cdn.logojoy.com/wp-content/uploads/2018/05/30164225/572.png" alt="blog-maker" />
+      {/* <img src={auth?.currentUser.photoURL} alt="" style={{width:"20%", borderRadius:"50%",marginRight:"1rem"}}/>
+      <h3>{auth?.currentUser.displayName}</h3> */}
       
       </div>
      <div className='email  d-flex justify-content-center align-items-center'style={{gap:"1rem"}}>
       {(location.pathname==='/Blogs') && (<Link to={"/AddBlogs"} className='btn btn-warning' >AddBlog</Link>)}
       {(location.pathname !=='/Blogs') && (<Link to={"/Blogs"} className='btn btn-warning' >BackToBlogs</Link>)}
-      <h3>{auth?.currentUser.email}</h3>
+      {/* <h3>{auth?.currentUser.email}</h3> */}
       <button
       onClick={logOut}
       className='btn btn-danger'>Log Out</button>
